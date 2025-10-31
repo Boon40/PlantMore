1df34115e2e158cae9e84720079224590295dcae
Yosif Yosifov <yosifyosifov0@gmail.com>
2025-10-31 15:45:17 +0100

    added buttons for edit char name, delete chat, set as favourite and create new chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,25 +35,33 @@ export default function App() {
       </header>
       <main className="layout">
         <aside className="sidebar" aria-label="Conversations">
-          <div className="sidebar-header">Chats</div>
+          <div className="sidebar-header">
+            <span>Chats</span>
+            <button className="iconSquare plus" type="button" aria-label="Start new chat" title="Start new chat" data-tip="Start new chat">
+              {/* Plus icon */}
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 5v14M5 12h14" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round"/>
+              </svg>
+            </button>
+          </div>
           <ul className="chat-list">
             <li className="chat-item active">
               Getting started
               <div className="chat-actions" aria-hidden="true">
-                <button className="iconSquare edit" title="Edit title" aria-label="Edit chat title" type="button">
+                <button className="iconSquare edit" title="Edit chat name" aria-label="Edit chat name" type="button" data-tip="Edit chat name">
                   {/* Pencil icon */}
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 17.25V21h3.75L19.81 7.94l-3.75-3.75L3 17.25z" stroke="currentColor" fill="none" strokeWidth="1.5"/>
                     <path d="M14.06 4.19l3.75 3.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
                   </svg>
                 </button>
-                <button className="iconSquare star" title="Favorite" aria-label="Favorite chat" type="button">
+                <button className="iconSquare star" title="Set chat as favourite" aria-label="Set chat as favourite" type="button" data-tip="Set chat as favourite">
                   {/* Star icon (outlined) */}
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27z" stroke="currentColor" fill="none" strokeWidth="1.5"/>
                   </svg>
                 </button>
-                <button className="iconSquare trash" title="Delete" aria-label="Delete chat" type="button">
+                <button className="iconSquare trash" title="Delete chat" aria-label="Delete chat" type="button" data-tip="Delete chat">
                   {/* Trash icon */}
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 6h18M9 6V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
@@ -65,18 +73,18 @@ export default function App() {
             <li className="chat-item">
               Cacti care
               <div className="chat-actions" aria-hidden="true">
-                <button className="iconSquare edit" title="Edit title" aria-label="Edit chat title" type="button">
+                <button className="iconSquare edit" title="Edit chat name" aria-label="Edit chat name" type="button" data-tip="Edit chat name">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 17.25V21h3.75L19.81 7.94l-3.75-3.75L3 17.25z" stroke="currentColor" fill="none" strokeWidth="1.5"/>
                     <path d="M14.06 4.19l3.75 3.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
                   </svg>
                 </button>
-                <button className="iconSquare star" title="Favorite" aria-label="Favorite chat" type="button">
+                <button className="iconSquare star" title="Set chat as favourite" aria-label="Set chat as favourite" type="button" data-tip="Set chat as favourite">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27z" stroke="currentColor" fill="none" strokeWidth="1.5"/>
                   </svg>
                 </button>
-                <button className="iconSquare trash" title="Delete" aria-label="Delete chat" type="button">
+                <button className="iconSquare trash" title="Delete chat" aria-label="Delete chat" type="button" data-tip="Delete chat">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 6h18M9 6V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
                     <path d="M10 11v6M14 11v6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
@@ -87,18 +95,18 @@ export default function App() {
             <li className="chat-item">
               Bloom boosters
               <div className="chat-actions" aria-hidden="true">
-                <button className="iconSquare edit" title="Edit title" aria-label="Edit chat title" type="button">
+                <button className="iconSquare edit" title="Edit chat name" aria-label="Edit chat name" type="button" data-tip="Edit chat name">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 17.25V21h3.75L19.81 7.94l-3.75-3.75L3 17.25z" stroke="currentColor" fill="none" strokeWidth="1.5"/>
                     <path d="M14.06 4.19l3.75 3.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
                   </svg>
                 </button>
-                <button className="iconSquare star" title="Favorite" aria-label="Favorite chat" type="button">
+                <button className="iconSquare star" title="Set chat as favourite" aria-label="Set chat as favourite" type="button" data-tip="Set chat as favourite">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27z" stroke="currentColor" fill="none" strokeWidth="1.5"/>
                   </svg>
                 </button>
-                <button className="iconSquare trash" title="Delete" aria-label="Delete chat" type="button">
+                <button className="iconSquare trash" title="Delete chat" aria-label="Delete chat" type="button" data-tip="Delete chat">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 6h18M9 6V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
                     <path d="M10 11v6M14 11v6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
